24e6101a7df33a04d0e7749b4c4acd6174a05dc7
Tobias Famos <tfamos@gmx.ch>
2023-04-24 10:09:17 +0200

    Add Green printing color

--- contiki_runicast/r_unicast.c
+++ contiki_runicast/r_unicast.c
@@ -79,7 +79,7 @@ static void recv_runicast(struct runicast_conn *c, rimeaddr_t *from, uint8_t seq
     int clockDifference =  clock_time() - (uint16_t)tmReceived.time;
     int secondDifference = clockDifference / CLOCK_SECOND;
     int msDifference = (1000 * ((uint16_t)clockDifference  % CLOCK_SECOND)) / CLOCK_SECOND;
-    printf("Timestamp Difference is %d clock cylces which relates to %d s %d ms\n",clockDifference, secondDifference, msDifference);
+    printf("Timestamp Difference is %d clock cylces which relates to \x1b[32m%d s %d ms \x1b[0m\n",clockDifference, secondDifference, msDifference);
   }
 }
 
